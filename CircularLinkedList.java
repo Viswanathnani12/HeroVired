4e3bc7954301c7c1d8b241a4dbbbe43da594b565
Viswanathnani12 <84368070+Viswanathnani12@users.noreply.github.com>
2023-02-20 23:59:38 +0530

    Update CircularLinkedList.java

--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -33,6 +33,35 @@ public class Main {
         }
         System.out.println(start.val);
     }
+    
+    public static Node AddtoStart(Node start,int x)
+    {
+        Node h=start;
+        Node n=new Node(x);
+        Node last = start;
+        n.next=h;
+        while (last.next != start) {
+            last = last.next;
+        }
+        last.next = n;
+        n.next = start;
+        start = n;
+        return start;
+    }
+
+    public static Node AddtoEnd(Node start,int x)
+    {
+        Node h=start;
+        Node n=new Node(x);
+        Node last = start;
+        while(last.next.next!=h)
+        {
+            last=last.next;
+        }
+        last.next=n;
+        n.next=h;
+        return start;
+    }    
     public static void main(String[] args) {
         int a[]={1,2,4,5,6,7,8};
         Node start=convert(a);
